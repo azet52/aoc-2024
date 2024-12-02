439f6303a8ca32d9c979edfa8a6d33487d6b0765
azet52 <azet52.nptr@gmail.com>
2024-12-02 22:27:22 +0100

    Solve first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,44 @@
+import kotlin.math.abs
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val aIds = mutableListOf<Int>()
+        val bIds = mutableListOf<Int>()
+
+        input.forEach { entry ->
+            val ids = entry.split("\\s+".toRegex()).map { it.toInt() }
+            aIds.add(ids[0])
+            bIds.add(ids[1])
+        }
+
+        aIds.sort()
+        bIds.sort()
+
+        return aIds.foldIndexed(0) { index, acc, id -> acc + abs(bIds[index] - id) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val aIds = mutableListOf<Int>()
+        val occurrencesById = mutableMapOf<Int, Int>()
+
+        input.forEach { entry ->
+            val ids = entry.split("\\s+".toRegex()).map { it.toInt() }
+            aIds.add(ids[0])
+            occurrencesById[ids[1]] = occurrencesById.getOrDefault(ids[1], 0) + 1
+        }
+
+        return aIds.fold(0) { acc, id ->
+            acc + (id * occurrencesById.getOrDefault(id, 0))
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+
+    check(part1(testInput) == 11)
     part1(input).println()
+
+    check(part2(testInput) == 31)
     part2(input).println()
 }
